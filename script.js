a0e1c72ee473234bb007d3aef055db060eb8a015
Matheus Oliveira Monteiro <matheusoliveira7227@gmail.com>
2023-10-23 13:26:57 -0300

    adding smooth scroll with js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const pageLinks = document.querySelectorAll(".links-footer a[href^='#']");
 
+pageLinks.forEach((item) => {
+  item.addEventListener("click", scrollOnClick);
+});
+
+function scrollOnClick(event) {
+  event.preventDefault();
+  const element = event.target;
+  const id = element.getAttribute("href");
+  const section = document.querySelector(id).offsetTop;
+
+  window.scroll({
+    top: section,
+    behavior: "smooth",
+  });
+}
